4d3e07022a2dc0ffda526f6bb6e54c34f7f6af18
Niklas <niklas.frank@gmail.com>
2015-12-03 09:04:19 +0100

    Added digit manipulation

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,24 @@
+/* global $ */
 var Calculator = function(){
 	this.n = [];
 	this.operator;
 	this.r = 0;
 }
 
+Calculator.prototype.number = function(a) {
+	this.r += a;
+	return this.r;
+}
 Calculator.prototype.addition = function(a, b) { return a+b; }
 Calculator.prototype.multiply = function(a, b) { return a*b; }
 Calculator.prototype.subtract = function(a,b) { return a-b; }
-Calculator.prototype.divide = function(a,b) {
-	if(b == 0) {
-		throw new Error("Error: Devide by zero");
-	}
-	return a/b;
-}
+Calculator.prototype.divide = function(a,b) { if(b == 0) throw new Error("Error: Devide by zero"); return a/b; }
+
+$(function() {
+	var calc = new Calculator();
+	
+	$("div#number").on('click', function() {
+		var a = calc.number(this.text());
+		$("#result").text(a);
+	});
+});
