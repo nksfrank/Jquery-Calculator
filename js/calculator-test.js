eebf77166ac2a57caf181602c14c34ee86b574ad
Niklas <niklas.frank@gmail.com>
2015-12-03 09:04:32 +0100

    Testing digit manipulation

--- js/calculator-test.js
+++ js/calculator-test.js
@@ -12,7 +12,17 @@ QUnit.test("Operands", function(assert) {
 	assert.equal(calc.multiply(0, 10), 0, "0");
 	assert.equal(calc.multiply(100, 0.1), 10, "10");
 	
-	assert.throws(function() { calc.divide(10, 0) }, new Error("Error: Devide by zero"), "NaN");
+	assert.throws(function() { calc.divide(10, 0) }, new Error("Error: Devide by zero"), "throws an error on zero devision");
 	assert.equal(calc.divide(10, 5), 2, "2");
 	assert.equal(calc.divide(1, 1), 1, "1");
+})
+
+QUnit.test("Number", function(assert) {
+	var calc = new Calculator();
+	assert.equal(calc.r, 0, "New calculator init as 0");
+	
+	assert.equal(calc.number(1), 1, "1");
+	assert.equal(calc.number(2), 3, "3");
+	assert.equal(calc.number(-1), 2, "2");
+	assert.equal(calc.number(0.1), 2.1, "2.1");
 })
